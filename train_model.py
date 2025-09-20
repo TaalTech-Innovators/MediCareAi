801b2c994cebc7d8c955f06fbdbd6702146909e7
Mapitsi Letsoalo <136266951+Mapitsi02@users.noreply.github.com>
2025-09-20 21:11:09 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -8,3 +8,7 @@ train_df = pd.read_csv("Training.csv")
 # Drop any unnamed columns
 train_df = train_df.loc[:, ~train_df.columns.str.contains("^Unnamed")]
 
+X = train_df.drop(columns=["prognosis"])
+y = train_df["prognosis"]
+
+print(" Training Random Forest model...")
