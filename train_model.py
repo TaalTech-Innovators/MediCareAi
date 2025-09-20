dd5403b992835f2540c64f2e766cc9656ae7d5d6
MbokaneK <karabombokane6@gmail.com>
2025-09-20 19:12:34 +0200

    Update train_model.py
    Added some codes

--- train_model.py
+++ train_model.py
@@ -2,3 +2,9 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 import joblib
+print(" Loading training data...")
+train_df = pd.read_csv("Training.csv")
+
+# Drop any unnamed columns
+train_df = train_df.loc[:, ~train_df.columns.str.contains("^Unnamed")]
+
