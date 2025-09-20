8b283eb78830222f90e8f6529840496b2ef81874
223215031 <223215031@edu.vut.ac.za>
2025-09-20 11:17:14 +0200

    Update Predict

--- predict.py
+++ predict.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import joblib
+
+# -----------------------------
+# 1. Load model and symptoms
+# -----------------------------
+clf = joblib.load("disease_model.pkl")
+
+train_df = pd.read_csv("Training.csv")
 
