86c77456b9ceb2dc5c86b06248f24bd866e08ebe
Mpho_Maahlo <224056107@edu.vut.ac.za>
2025-09-21 20:11:03 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,6 +2,9 @@ import argparse
 import pandas as pd
 import joblib
 import config
+from data_loader import load_data
+from model import load_model
+
 
 # -----------------------------
 # 1. Load model and symptoms
