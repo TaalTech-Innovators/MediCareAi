543817d24d1317138c8bbb899c483def65d156cd
Mpho_Maahlo <224056107@edu.vut.ac.za>
2025-09-21 21:30:17 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,6 +4,10 @@ import joblib
 import config
 from data_loader import load_data
 from model import load_model
+def main(model_path, input_file, output_file):
+    model = load_model(model_path)
+ le = joblib.load(config.MODELS_DIR / "label_encoder.joblib")
+
 
 
 
