59cefa21243be572938929d4cb5840a1b4fa9c0b
Shivambu17 <Shivambushaun0@gmail.com>
2025-09-23 00:32:27 +0200

    retraived
    for a wrong file that i was not working on

--- predict.py
+++ predict.py
@@ -6,21 +6,13 @@ import matplotlib.pyplot as plt
 from tabulate import tabulate
 
 
-# 1. Load model and symptoms
+def main(model_path, input_file, output_file):
+    model = load_model(model_path)
+le = joblib.load(config.MODELS_DIR / "label_encoder.joblib")
 
-train_df = pd.read_csv("Training.csv")
-train_df = train_df.loc[:, ~train_df.columns.str.contains('^Unnamed')]
-symptoms = list(train_df.drop(columns=["prognosis"]).columns)
-
-
-
-#def main(model_path, input_file, output_file):
-   # model = load_model(model_path)
-#le = joblib.load(config.MODELS_DIR / "label_encoder.joblib")
-
-#df = load_data(input_file)
-#if config.TARGET_COLUMN in df.columns:
- #df = df.drop(columns=[config.TARGET_COLUMN])
+df = load_data(input_file)
+if config.TARGET_COLUMN in df.columns:
+ df = df.drop(columns=[config.TARGET_COLUMN])
 
 
 
