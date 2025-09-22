f07df3f48c8851c5e70161d1436da4a75f0862ef
Shivambu17 <Shivambushaun0@gmail.com>
2025-09-23 00:15:48 +0200

    upades

--- predict.py
+++ predict.py
@@ -1,17 +1,26 @@
-import argparse
+
 import pandas as pd
 import joblib
-import config
-from data_loader import load_data
-from model import load_model
+import random
+import matplotlib.pyplot as plt
+from tabulate import tabulate
 
-def main(model_path, input_file, output_file):
-    model = load_model(model_path)
-le = joblib.load(config.MODELS_DIR / "label_encoder.joblib")
 
-df = load_data(input_file)
-if config.TARGET_COLUMN in df.columns:
- df = df.drop(columns=[config.TARGET_COLUMN])
+# 1. Load model and symptoms
+
+train_df = pd.read_csv("Training.csv")
+train_df = train_df.loc[:, ~train_df.columns.str.contains('^Unnamed')]
+symptoms = list(train_df.drop(columns=["prognosis"]).columns)
+
+
+
+#def main(model_path, input_file, output_file):
+   # model = load_model(model_path)
+#le = joblib.load(config.MODELS_DIR / "label_encoder.joblib")
+
+#df = load_data(input_file)
+#if config.TARGET_COLUMN in df.columns:
+ #df = df.drop(columns=[config.TARGET_COLUMN])
 
 
 
