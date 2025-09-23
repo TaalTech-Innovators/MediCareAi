1cead8e93e1df564f6979154b783a50215ce0328
Mapitsi Letsoalo <136266951+Mapitsi02@users.noreply.github.com>
2025-09-23 14:55:44 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,4 @@
 import argparse
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import classification_report, accuracy_score
-import joblib
-
-import config
 
