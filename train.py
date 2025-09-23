61902261f26c27d4843e3d8d7190605d3001eb2c
Mapitsi Letsoalo <136266951+Mapitsi02@users.noreply.github.com>
2025-09-23 14:17:03 +0200

    Delete train.py

--- train.py
+++
@@ -1,9 +0,0 @@
-import argparse
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import classification_report, accuracy_score
-import joblib
-
-import config
-from data_loader import load_data
-from model import build_model, save_model
