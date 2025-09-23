5240fd9e015ed7adea6b8fc3b51eb3ffed137d9f
MbokaneK <karabombokane6@gmail.com>
2025-09-23 13:43:33 +0200

    deleting the old file

--- training.py
+++ training.py
@@ -1,21 +1,2 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-import joblib
-print(" Loading training data...")
-train_df = pd.read_csv("Training.csv")
 
-# Drop any unnamed columns
-train_df = train_df.loc[:, ~train_df.columns.str.contains("^Unnamed")]
-
-X = train_df.drop(columns=["prognosis"])
-y = train_df["prognosis"]
-
-print(" Training Random Forest model...")
-
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
-clf.fit(X, y)
-
-scores =cross_val_score(clf, X,y,cv=5)
-print(f"Cross-validation Accuracy: {scores.mean():.4f}")
 
