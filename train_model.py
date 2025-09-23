bddad14da18bb2e312ee4f2b0a9c4eea762e4981
Ms Batseba Leshilo <223215031@edu.vut.ac.za>
2025-09-23 13:28:41 +0200

    Delete train_model.py

--- train_model.py
+++
@@ -1,4 +0,0 @@
-import argparse
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import classification_report, accuracy_score
