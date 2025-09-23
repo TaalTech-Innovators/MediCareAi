247b28e17054ca498971b843c01ed008408bd072
KamoMolamu <kamogelomolamu172@gmail.com>
2025-09-23 17:29:01 +0200

    Add data loading step in test_model.py
    Added line to load the test dataset with target column

--- test_model.py
+++ test_model.py
@@ -1,9 +1,12 @@
 import pandas as pd #by kgotso (corrected)
+import argparse #by kamo
 import joblib #by kgotso (corrected)
 from sklearn.metrics import classification_report, accuracy_score #by kgotso (corrected)
 import config                   # by Kamogelo
 from data_loader import load_data        # by Kamogelo
 from model import load_model             # by Kamogelo
+
 def main(model_path, test_file): #by Angela
 model = load_model(model_path)  #by Angela
 le = joblib.load(config.MODELS_DIR / "label_encoder.joblib") #by Angela
+df = load_data(test_file, config.TARGET_COLUMN) #by kamo
